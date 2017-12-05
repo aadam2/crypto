d82a65e52a521c4bbd9c4cd7443005424459b08c
alaingrd <glavom@gmail.com>
2017-12-06 00:20:44 +0100

    Rounds keys generated

--- tf-cs-cipher.py
+++ tf-cs-cipher.py
@@ -1,4 +1,6 @@
 # Generic functions
+################################################################################
+
 def tobits(s): # Converts characters into a list of bits
     result = []
     for c in s:
@@ -41,42 +43,79 @@ def xoring_list_of_lists(subkeys_list):
     for i in range(1, nb_lists):
         print("Having i = {0}".format(i))
         last_subkey = xoring_two_lists(last_subkey,subkeys_list[i])
-    print("Last subkey (without C yet) : {0}".format(last_subkey))
+    #print("Last subkey (without C yet) : {0}".format(last_subkey))
 
     C = [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0]
     last_subkey = xoring_two_lists(last_subkey, C)
-    print("Last subkey (AFTER C XOR) : {0}".format(last_subkey))
+    #print("Last subkey (AFTER C XOR) : {0}".format(last_subkey))
     return last_subkey
 
-# Functions definitions
+################################################################################
 
+# Functions definitions
+################################################################################
 # ThreeFish related
-def threefish_key_schedule(key, block_size):
+def threefish_key_schedule(key, block_size): # Generates the original keywords list (used for the first round)
+
+    # Computing the three tweaks
+    t0 = key[:64]# First 64 bits from the key
+    t1 = key[64:128]# Following 64 bits of the key
+    t2 = xoring_two_lists(t0, t1)
+    tweaks = []
+    tweaks.append(t0)
+    tweaks.append(t1)
+    tweaks.append(t2)
+
+    # Computing number of keywords
     nb_key_words = block_size / 64
     print("Number of key words : {0}".format(nb_key_words))
     keywords_list = divide_list(key, nb_key_words)
-    print("Keywords list : {0}".format(keywords_list))
 
     # Now adding kN : kN = k0 ^ k1 ^ ... ^ k_N-1 ^ C
     last_subkey = xoring_list_of_lists(keywords_list)
-    #print("Received last subkey : {0}".format(last_subkey))
 
     # Appending the last subkey to the key
     keywords_list.append(last_subkey)
-    #print("(Almost) Complete key words list : {0}".format(keywords_list))
-    #print("(Almost) Complete key words list size : {0}".format(len(keywords_list)))
-    return keywords_list
 
+    # keywords_list now contains the original keywords list (used for the first round)
+    round_list = []
+    rounds_keywords_list = [] # List of lists of lists, keywords list for each round (Round->Keywords_List->Keyword)
+    # Doing the first round
+
+    N = nb_key_words
+    print("[2] - N = {0}".format(N))
+    print("[3] - keywords_list length = {0}".format(len(keywords_list)))
+    for i in range(20): # Browsing the rounds
+        for n in range(N-3): # Browsing the blocks
+            current_index = (n + i) % (N + 1)
+            print ("With i = {0} and n = {1}, we have current_index = {2}".format(i, n, current_index))
+            #round_list[n] = keywords_list[(n + i) % (N + 1)]
+            round_list.append(keywords_list[(n + i) % (N + 1)])
+        print("[4] - round_list so far : {0}".format(round_list))
+        # round_list[N-3] = xoring_two_lists(keywords_list[(N - 3 + i) % (N + 1)], tweaks[(i % 3)])
+        round_list.append(xoring_two_lists(keywords_list[(N - 3 + i) % (N + 1)], tweaks[(i % 3)])) # N-3
+        #round_list[N-2] = xoring_two_lists(keywords_list[(N - 2 + i) % (N + 1)], tweaks[((i + 1) % 3)])
+        round_list.append(xoring_two_lists(keywords_list[(N - 2 + i) % (N + 1)], tweaks[((i + 1) % 3)])) # N-2
+        #round_list[N-1] = keywords_list[(N - 1 + i) % (N + 1)] # TODO Do the correction modular addition here (with i)
+        round_list.append(keywords_list[(N - 1 + i) % (N + 1)]) # TODO Do the correction modular addition here (with i). Anyway, N-1 here
+        rounds_keywords_list.append(round_list)
+
+    return rounds_keywords_list
 
 def threefish_encrypt(key, msg_bits, block_size):
-    keywords_list = threefish_key_schedule(key, block_size) # Generating the key words
-    print("The original keywords list is shown below")
-    print(keywords_list)
+    rounds_keywords_list = threefish_key_schedule(key, block_size) # Generating the key words
+    print("Keywords list for the different rounds : ")
+    print(rounds_keywords_list)
 
+
+    print("rounds_keywords_list[0] : {0}".format(rounds_keywords_list[0])) # Keywords for round 1
+    print("len(rounds_keywords_list) : {0}".format(len(rounds_keywords_list)))
+    print("len(rounds_keywords_list[0]) : {0}".format(len(rounds_keywords_list[0])))
 
     return
 
-
+# Main function
+################################################################################
 # Calling user defined functions
 def main():
     print("Select your encryption function")
@@ -142,8 +181,9 @@ def main():
 
         # Now that the key size and the input size are OK, we may continue
         print("[1] - key_bits = {0} ; bits_to_encrypt = {1} ; block_size = {2}".format(len(key_bits), len(bits_to_encrypt), block_size))
-        print("Key bits used : {0}".format(key_bits))
+        #print("Key bits used : {0}".format(key_bits))
         threefish_encrypt(key_bits, bits_to_encrypt, block_size)
 
 if __name__ == "__main__":
     main()
+################################################################################
